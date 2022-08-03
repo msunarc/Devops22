5fffc44c550a0f146e6fef67fcd85d109550751f
msunarc <66220066+msunarc@users.noreply.github.com>
2022-08-02 21:53:03 -0400

    Add files via upload

--- task-2/main.tf
+++ task-2/main.tf
@@ -8,5 +8,15 @@ terraform {
 
   required_version = ">= 0.14.9"
 }
-
-
+resource "aws_instance" "task1" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name = "virKP"
+  vpc_security_group_ids = [aws_security_group.MyLab_Sec_Group.id]
+ 
+  associate_public_ip_address = true
+  user_data = file("tomcatTF.sh")
+}
+  tags = {
+    Name = "softsware-Server"
+  }
